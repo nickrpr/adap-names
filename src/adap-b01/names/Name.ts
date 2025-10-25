279b7d9de9863aedbcf1c5976f7fb35e21edcaa3
Nick Rupprecht <nick.rupprecht@fau.de>
2025-10-25 03:42:22 +0200

    few tests and change to default delimeter in asDataString

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_DELIMITER: string = '.';
-export const ESCAPE_CHARACTER = '\\';
+export const ESCAPE_CHARACTER = '\\';  // --> console.log(ESCAPE_CHARACTER) = \
 
 /**
  * A name is a sequence of string components separated by a delimiter character.
@@ -19,13 +19,15 @@ export class Name {
     private components: string[] = [];
 
     /** Expects that all Name components are properly masked */
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
+        console.log("other:", other);
+        console.log("\\")
         this.components = other.slice();
+        console.log("components:", this.components);
         if (delimiter) {
             this.delimiter = delimiter;
         }
-
-        // throw new Error("needs implementation or deletion");
     }
 
     /**
@@ -35,11 +37,16 @@ export class Name {
      */
     // e.g.: const n = new Name(["oss", "cs", "fau", "de"]);
     // n.asString(); // Ergebnis: "oss.cs.fau.de"
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
         return this.components.join(delimiter); //TODO: whs muss ich das hier noch ändern und etwas maskieren, also whs noch falsch, nochmal drüber nachdenken!! wegen control chars are not escaped
-        // throw new Error("needs implementation or deletion");
     }
     // TODO: ["oss\\", "cs", "fau\.", "de"].asString() --> Ergebnis: "oss\\.cs.fau\..de" --> wäre das so korrekt?
+    /**
+     * Interner Speicher (this.components): ["oss", "cs", "fau", "de"]
+     * n.asString() gibt zurück: "oss.cs.fau.de"
+     * n.asDataString() gibt zurück: "oss.cs.fau.de" (Hier gibt es nichts zu maskieren, da keine Sonderzeichen in den Komponenten sind).
+     */
 
 
 
@@ -48,7 +55,10 @@ export class Name {
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The special characters in the data string are the default characters --> daher habe ich unten DEFAULT_DELIMETER statt this.delimeter verwendet
      */
+    // TODO: ANNNOTE EACH METHOD siehe vorlesungsfolien zu homework, also zb @methodtype create-method
+    // @methodtype conversion-method
     public asDataString(): string {
+        console.log("ACHTUNG hier:", this.components);
         // check whether a control character is present inside each string, and if so, escape each control character, such that they stay inside the data string and the data string gets machine readable
         // => "fau\.de.cs" --> Der Punkt nach fau soll also kein Trennzeichen sein, sondern ist Teil der Komponente! --> also ["fau.de", "cs"]
         const escapedComponents = this.components.map(component => {
@@ -56,54 +66,54 @@ export class Name {
                 .replaceAll(ESCAPE_CHARACTER, ESCAPE_CHARACTER + ESCAPE_CHARACTER)  // z.B.: \\ wird zu \\\\
                 .replaceAll(DEFAULT_DELIMITER, ESCAPE_CHARACTER + DEFAULT_DELIMITER);     // z.B.: . wird zu \.
         });
-        return escapedComponents.join(this.delimiter);
-        // throw new Error("needs implementation or deletion");
+        return escapedComponents.join(DEFAULT_DELIMITER);
     }
 
+    /** Returns properly masked component string */
+    // @methodtype get-method
     public getComponent(i: number): string {
         if (i < 0 || i >= this.components.length) {
             throw new Error("Index " + i + " is out of bounds");
         }
         return this.components[i];
-        // throw new Error("needs implementation or deletion");
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
         if (i < 0 || i >= this.components.length) {
             throw new Error("Index " + i + " is out of bounds");
         }
         this.components[i] = c;
-        // throw new Error("needs implementation or deletion");
     }
 
     /** Returns number of components in Name instance */
+    // @methodtype get-method
     public getNoComponents(): number {
         return this.components.length;
-        // throw new Error("needs implementation or deletion");
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         if (i < 0 || i > this.components.length) {
             throw new Error("Index " + i + " is out of bounds")
         }
-        this.components.splice(i, 0, c)
-        // throw new Error("needs implementation or deletion");
+        this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public append(c: string): void {
         this.components.push(c);
-        // throw new Error("needs implementation or deletion");
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         if (i < 0 || i >= this.components.length) {
             throw new Error("Index " + i + " is out of bounds");
         }
         this.components.splice(i, 1);
-        // throw new Error("needs implementation or deletion");
     }
 
 }
